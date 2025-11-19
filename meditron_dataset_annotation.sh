f1d2527652725329e8a583edc87faa1b7c6395ab
Miki Vanoušek <vanousekmikulas@gmail.com>
2025-11-19 14:42:54 +0100

    wip

---
+++ meditron_dataset_annotation.sh
@@ -0,0 +1,8 @@
+uv run python -m annotation_pipeline.run \
+    --hf_dataset_name MikiV/pubmedqa-meditron-conversations-labeled \
+    --hf_dataset_split "train[:3]" \
+    --hf_dataset_subset "" \
+    --output_hf_dataset_name "MikiV/pubmedqa-meditron-conversations-annotated" \
+    --output_hf_dataset_subset "" \
+    --output_hf_dataset_split "test" \
+    --model_id "claude-sonnet-4-5-20250929"
